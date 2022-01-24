c64a42ab9703a4e282c341ced09ded203d5e3955
Loïc SIMEON <loicsimeon@MacBook-Air-de-Loic.local>
2022-01-24 14:37:31 +0100

    fix des dates

--- pricer_skel/scripts/printCurve.py
+++ pricer_skel/scripts/printCurve.py
@@ -4,11 +4,10 @@ import pandas as pd
 import sys
 
 argtitle = sys.argv[1]
-x = pd.read_csv("../build/dates.txt", header = None)
-plt.rcParams["figure.figsize"] = (200, 200)
+x = pd.read_csv("../build/dates.txt", header = None, parse_dates = [0])
 if( x[0].dtypes != "double" ):
     pd.to_datetime(x[0])
-
+print(x.dtypes)
 for i, arg in enumerate(sys.argv):
     if i >=1:
         data = pd.read_csv(arg, header = None)
